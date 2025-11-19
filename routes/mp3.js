c998bbfe805d52496e53982301d1e90835d9cdb0
Erick <zenopubg011@gmail.com>
2025-11-19 21:21:04 +0545

    Delete routes/mp3.js

--- routes/mp3.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { handleYouTubeDownload } = require("../controllers/mp3Controller");
-
-router.get("/download", handleYouTubeDownload);
-
-module.exports = router;
